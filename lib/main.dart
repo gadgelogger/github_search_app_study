4499bda37cf39f6ab71af383bc006926396334e1
gadgelogger <sarayou273142910@gmail.com>
2023-07-11 15:38:38 +0900

    APIとの通信部分を作成

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'github_search_app_study',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -25,31 +24,20 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
-
-
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
         title: const Text('github'),
       ),
       body: const Center(
-
         child: const Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: const <Widget>[
             const Text(
